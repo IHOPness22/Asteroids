43acb753d846b5184d6574e28acb33d3f6f182c2
IHOPness22 <Jescoto046@student.glendale.edu>
2025-02-18 10:25:44 -0800

    Add asteroid class and field implementation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
-from constants import *  
+from constants import *
+from player import Player  
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     pygame.init()
@@ -7,12 +10,32 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}" )
     print(f"Screen height: {SCREEN_HEIGHT}" )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    running = True	
+    while running:
+        dt = clock.tick(60) / 1000
+       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-            screen.fill((0, 0, 0))
-            pygame.display.flip()    
+                running = False
+        updatable.update(dt)
+        screen.fill("black")
+        for sprite in drawable:
+            sprite.draw(screen)
+        pygame.display.flip()
+        
+
 
 if __name__ == "__main__":
     main()
